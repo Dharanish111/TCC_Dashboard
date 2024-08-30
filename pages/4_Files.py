53c3ea8b240a70c1d268b3102541bceb95e53242
Pinnamshetty Dharanish <168800443+Dharanish111@users.noreply.github.com>
2024-08-30 12:43:05 +0530

    Update 4_Files.py

--- pages/4_Files.py
+++ pages/4_Files.py
@@ -100,7 +100,7 @@ uploads_count = recordings_df.groupby('interval').size().reset_index(name='count
 
 col1,col2,col3 = st.columns([3,1,3])
 with col2:
-    image_1 = st.image("https://raw.githubusercontent.com/Dharanish111/TCC_Dashboard/main/1.png")
+    image_1 = st.image("https://raw.githubusercontent.com/Dharanish111/TCC_Dashboard/main/Pics/1.png")
     
 st.title("Files 📂")
 # Display results
